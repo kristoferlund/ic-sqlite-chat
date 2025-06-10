a87e5d4afd16bf98ddefcc5333049ddbada5c852
wasm-forge <122647775+wasm-forge@users.noreply.github.com>
2025-06-10 12:14:15 +0200

    update dependencies

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@ create-canisters:
 	dfx canister create --all
 
 deploy-backend:
-	export CC_wasm32_wasi="/opt/wasi-sdk/bin/clang" && \
-    export CFLAGS_wasm32_wasi="--sysroot=/opt/wasi-sdk/share/wasi-sysroot" && \
-	cargo build --release --target wasm32-wasi && \
-	cd ./target/wasm32-wasi/release && \
+	export CC_wasm32_wasip1="/opt/wasi-sdk/bin/clang" && \
+    export CFLAGS_wasm32_wasip1="--sysroot=/opt/wasi-sdk/share/wasi-sysroot" && \
+	cargo build --release --target wasm32-wasip1 && \
+	cd ./target/wasm32-wasip1/release && \
 	wasi2ic backend.wasm backend.wasm && \
 	candid-extractor backend.wasm > ../../../src/backend/backend.did && \
 	ic-wasm backend.wasm -o backend.wasm metadata candid:service -f ../../../src/backend/backend.did -v public && \
--- src/backend/build.rs
+++ src/backend/build.rs
@@ -1,4 +1,24 @@
 fn main() {
-    println!("cargo:rustc-link-search=/opt/wasi-sdk/lib/clang/18/lib/wasi/");
-    println!("cargo:rustc-link-arg=-lclang_rt.builtins-wasm32");
+    // locate wasi builtins
+    let sdk_path = if let Ok(sdk_path) = std::env::var("WASI_SDK") {
+        sdk_path
+    } else {
+        "/opt/wasi-sdk".to_string()
+    };
+
+    let pattern = format!("{sdk_path}/lib/clang/*/lib/wasip1");
+
+    let paths: Vec<std::path::PathBuf> = glob::glob(&pattern)
+        .expect("Failed to read glob pattern")
+        .filter_map(Result::ok)
+        .collect();
+
+    if let Some(path) = paths.last() {
+        // use the latest version we have found
+
+        println!("cargo:rustc-link-search={}", path.display());
+        println!("cargo:rustc-link-arg=-lclang_rt.builtins-wasm32");
+    } else {
+        panic!("Could not find clang wasm32 builtins under '{}', make sure wasi-sdk is installed, see https://github.com/WebAssembly/wasi-sdk/releases.", pattern);
+    }
 }
